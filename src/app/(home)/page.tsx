60c5b1bea5b87a8cc56d977021462b8287fa91fc
Willian Bruno <willian.bruno21@outlook.com>
2023-10-23 22:28:03 -0300

    feat: add PromoBanner component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,9 @@
 "use client";
 
-import { useSession } from 'next-auth/react';
-import Image from 'next/image'
+import PromoBanner from '@/components/ui/PromoBanner';
+
 
 export default function Home() {
   return (
-  <div className='p-5'>
-    <Image 
-      src="/banner01.png"
-      height={0}
-      width={0}
-      className='h-auto w-full'
-      sizes='100w'
-      alt='Até 55% de desconto esse mês!'
-    />
-  </div>
+  <PromoBanner src="/banner01.png" />
 )}
